81d6945707ce2dc9c07db2c7a434b30b46c2d643
mariela.racheva <mimitofifi@gmail.com>
2022-05-20 09:59:06 +0300

    Add project files.

---
+++ WebDriverTestsDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+// Create new chrome browser instance
+var driver = new ChromeDriver();
+
+// Navigate to Wikipedia
+driver.Url = "https://wikipedia.org";
+
+// Locate search Field by ID
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+// click on element
+searchField.Click();
+
+// Find QA and press Enter keyboard button
+searchField.SendKeys("Quality Assuarance" + Keys.Enter);
+
+System.Console.WriteLine("Title after search: " + driver.Title);
+// Close browser
+driver.Quit();
